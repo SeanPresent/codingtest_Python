c8fd17201fda6e09f65db0d338ad62d1780c639d
Seung.H <90368764+SeanPresent@users.noreply.github.com>
2023-02-14 10:36:56 +0900

    Update [Hash_Table]anagram_validation.py

--- Coding_Techniques/[Hash_Table]anagram_validation.py
+++ Coding_Techniques/[Hash_Table]anagram_validation.py
@@ -6,3 +6,33 @@ eg) s1 = "garden", s2 = "danger"
 """
 
 # First Method
+
+def are_anagrams(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    freq1 = {}
+    freq2 = {}
+    for ch in s1:
+        if ch in freq1:
+            freq1[ch] += 1
+        else:
+            freq1[ch] = 1
+    for ch in s2:
+        if ch in freq2:
+            freq2[ch] += 1
+        else :
+            freq2[ch] = 1
+    for key in freq1:
+        if key not in freq2 or freq1[key] != freq2[key]:
+            return False
+          
+# with counter method
+
+from collections import Counter
+def are_anagram(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    return Counter(s1) == Counter(s2)
+
+    
+         
